265601b4b7cce21e0ac8e2922916681f0b7f8d3b
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2025-06-18 21:09:08 +0530

    Created a Post.java model class.

--- insta-clone-backend/src/main/java/com/instaclone/model/Post.java
+++ insta-clone-backend/src/main/java/com/instaclone/model/Post.java
@@ -1,7 +1,21 @@
 package com.instaclone.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.sql.Timestamp;
 
 @Entity
+@Data
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String imgPath;
+    private String imgCaption;
+    private Timestamp createdAt = new Timestamp(System.currentTimeMillis());
 }
