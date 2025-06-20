8b8eaeb509025b1d2b1fface27e52b4ca557b186
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2025-06-20 13:41:53 +0530

    Updated PostService interface and declared abstract method getAllPost.

--- insta-clone-backend/src/main/java/com/instaclone/service/PostService.java
+++ insta-clone-backend/src/main/java/com/instaclone/service/PostService.java
@@ -1,8 +1,12 @@
 package com.instaclone.service;
 
+import com.instaclone.model.Post;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 public interface PostService {
     boolean addPost(String imgPath, String imgCaption);
+    List<Post> getAllPost();
 }
