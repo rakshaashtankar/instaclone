eb6b5f2d6fe153a7b2e7e444e50c1c6062cdf8f0
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2025-06-20 13:25:52 +0530

    Created and updated PostServiceImpl class and defined abstract method addPost declared in PostService interface.

--- insta-clone-backend/src/main/java/com/instaclone/service/PostServiceImpl.java
+++ insta-clone-backend/src/main/java/com/instaclone/service/PostServiceImpl.java
@@ -1,4 +1,34 @@
 package com.instaclone.service;
 
-public class PostServiceImpl {
+import com.instaclone.model.Post;
+import com.instaclone.repository.PostRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+@AllArgsConstructor
+public class PostServiceImpl implements PostService{
+
+    private final PostRepository postRepository;
+
+    @Override
+    public boolean addPost(String imgPath, String imgCaption) {
+        try{
+            if (Objects.nonNull(imgPath) && !imgPath.trim().isEmpty()
+                && Objects.nonNull(imgCaption) && !imgCaption.trim().isEmpty()) {
+                Post newPost = new Post();
+                newPost.setImgPath(imgPath);
+                newPost.setImgCaption(imgCaption);
+                postRepository.save(newPost);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (NullPointerException e) {
+            throw new RuntimeException("Data insufficient." + e.getMessage(), e);
+        }
+    }
 }
